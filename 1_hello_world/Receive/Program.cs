401bd8a5eb5d082d0be95b878189498d9eb9a757
sayeed1999 <mdsayeedrahman1999@gmail.com>
2024-06-29 16:53:39 +0600

    feat: add proper tutorial on message acknowledgement manually on consumer dying.

--- 1_hello_world/Receive/Program.cs
+++ 1_hello_world/Receive/Program.cs
@@ -19,10 +19,20 @@ consumer.Received += (model, ea) =>
 {
     var body = ea.Body.ToArray();
     var message = Encoding.UTF8.GetString(body);
+
+    Thread.Sleep(3000);
     Console.WriteLine($" [x] Received {message}");
+
+    // Note: we are manually acknowledging rabbitmq that message processing is done.
+    // What happens here is if a consumer dies before acknowleding, rabbitmq will requeue it.
+    // To test it, comment out line 29, publish and message, and turn off consumer application to see the message is requeued.
+    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
 };
 channel.BasicConsume(queue: "hello",
-                     autoAck: true,
+                    //  autoAck: true,
+                    // Note: Telling rabbitmq to not auto delete the message from queue on consumer received, rather wait for manual acknowledgement.
+                    // So that If any exception occurs and consumer fails, to finish its task, the message wont be lost.
+                     autoAck: false,
                      consumer: consumer);
 
 Console.WriteLine(" Press [enter] to exit.");
